a353c1858d796b56091a25e51258b486274c71df
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2022-07-24 15:20:58 +0200

    Refactor stuff and improve options/args parsing

--- hubmon.js
+++ hubmon.js
@@ -1,94 +1,27 @@
 #!/usr/bin/env node
 
-import chokidar from 'chokidar';
 import debounceFn from 'debounce-fn';
-import minimist from 'minimist';
 import chalk from 'chalk';
-import { spawn } from 'child_process';
+import { getOptionsAndCommand, startProcess } from './lib.js';
 
-// SETUP
-const hubmonOptions = minimist(process.argv.slice(2));
-const [commandName, ...commandArgs] = hubmonOptions._;
-const watchPattern = hubmonOptions.watch ?? '**/*';
-
-// GLOBAL STATE
+// Global state
 let iteration = -1;
-let commandProcess;
+let stopProcess = () => null;
 
-// Start the command in another process with childProcess.spawn()
-function startProcess () {
+const { watchPattern = '**/*', commandName, commandArgs } = getOptionsAndCommand();
 
+// Debounce to avoid restarting too much with multiple consecutive file changes
+const startProcessDebounced = debounceFn(() => {
   iteration += 1;
-  commandProcess = spawn(commandName, commandArgs);
-
-  const date = new Date().toISOString()
-    .slice(0, 'XXXX-XX-XXTXX:XX:XX'.length)
-    .replace('T', '  ');
-
-  console.log([
-    chalk.yellow(`[hubmon] ${date}`),
-    chalk.yellow(`${chalk.white('pid:')}${commandProcess.pid}`),
-    chalk.yellow(`${chalk.white('iteration:')}${iteration}`),
-  ].join('  '));
-
-  commandProcess.stdout.on('data', (data) => {
-    process.stdout.write(data.toString());
-  });
-
-  commandProcess.stderr.on('data', (data) => {
-    process.stderr.write(data.toString());
-  });
-
-  commandProcess.on('close', () => {
-    endProcess();
-  });
-}
-
-// Cleanup listeners, log and force kill process
-function endProcess () {
-
-  if (commandProcess == null) {
-    return;
-  }
-
-  const { exitCode } = commandProcess;
-
-  // Remove all event listneners (not sure if it's required)
-  commandProcess.stdout.removeAllListeners('data');
-  commandProcess.stderr.removeAllListeners('data');
-  commandProcess.removeAllListeners('close');
-
-  if (exitCode == null) {
-    console.log(chalk.red(`[hubmon] process killed due to file change`));
-  }
-  else if (exitCode === 0) {
-    console.log(chalk.green(`[hubmon] process stopped (${commandProcess.exitCode})`));
-  }
-  else {
-    console.log(chalk.red(`[hubmon] process stopped (${commandProcess.exitCode})`));
-  }
-
-  commandProcess.kill();
-  commandProcess = null;
-}
-
-// Watch file system according to watch pattern option
-const watcher = chokidar.watch(watchPattern, {
-  // ignore dotfiles
-  ignored: /(^|[\/\\])\../,
-  persistent: true,
-  ignoreInitial: true,
-});
-
-// Avoid restarting too much with multiple file changes
-const startProcessDebounced = debounceFn(startProcess, { wait: 100 });
+  stopProcess = startProcess(commandName, commandArgs, iteration);
+}, { wait: 100 });
 
 // On each file change, try to stop process, log and eventually start a new process
-watcher.on('all', (event, path, stats) => {
-  endProcess();
+watchFiles(watchPattern, (event, path) => {
+  stopProcess();
   console.log(chalk.yellow(`[hubmon] (${event}) ${path}`));
   startProcessDebounced();
 });
 
 // First start
-startProcess();
+startProcessDebounced();
---
+++ lib.js
@@ -0,0 +1,94 @@
+#!/usr/bin/env node
+
+import chalk from 'chalk';
+import { spawn } from 'child_process';
+import chokidar from 'chokidar';
+
+// Dead simple parse arguments for `--watch` or `-w` at the beginning of the command
+export function getOptionsAndCommand () {
+  const args = process.argv.slice(2);
+  const hasWatchArgument = args[0] === '--watch' || args[0] === '-w';
+  if (hasWatchArgument) {
+    return {
+      watchPattern: args[1],
+      commandName: args[2],
+      commandArgs: args.slice(3),
+    };
+  }
+  return {
+    commandName: args[0],
+    commandArgs: args.slice(1),
+  };
+}
+
+// Start the command in another process with childProcess.spawn()
+export function startProcess (commandName, commandArgs, iteration) {
+
+  let commandProcess = spawn(commandName, commandArgs);
+
+  function endProcess () {
+
+    if (commandProcess == null) {
+      return;
+    }
+
+    const { exitCode } = commandProcess;
+
+    // Remove all event listneners (not sure if it's required)
+    commandProcess.stdout.removeAllListeners('data');
+    commandProcess.stderr.removeAllListeners('data');
+    commandProcess.removeAllListeners('close');
+
+    if (exitCode == null) {
+      console.log(chalk.red(`[hubmon] process killed due to file change`));
+    }
+    else if (exitCode === 0) {
+      console.log(chalk.green(`[hubmon] process stopped (${commandProcess.exitCode})`));
+    }
+    else {
+      console.log(chalk.red(`[hubmon] process stopped (${commandProcess.exitCode})`));
+    }
+
+    commandProcess.kill();
+    commandProcess = null;
+  }
+
+  const date = new Date().toISOString()
+    .slice(0, 'XXXX-XX-XXTXX:XX:XX'.length)
+    .replace('T', '  ');
+
+  console.log([
+    chalk.yellow(`[hubmon] ${date}`),
+    chalk.yellow(`${chalk.white('pid:')}${commandProcess.pid}`),
+    chalk.yellow(`${chalk.white('iteration:')}${iteration}`),
+  ].join('  '));
+
+  commandProcess.stdout.on('data', (data) => {
+    process.stdout.write(data.toString());
+  });
+
+  commandProcess.stderr.on('data', (data) => {
+    process.stderr.write(data.toString());
+  });
+
+  commandProcess.on('close', () => {
+    endProcess();
+  });
+
+  // Cleanup listeners, log and force kill process
+  return endProcess;
+}
+
+export function watchFiles (watchPattern, callback) {
+
+  // Watch file system according to watch pattern option
+  const watcher = chokidar.watch(watchPattern, {
+    // ignore dotfiles
+    ignored: /(^|[\/\\])\../,
+    persistent: true,
+    ignoreInitial: true,
+  });
+
+  // On each file change, try to stop process, log and eventually start a new process
+  watcher.on('all', callback);
+}
